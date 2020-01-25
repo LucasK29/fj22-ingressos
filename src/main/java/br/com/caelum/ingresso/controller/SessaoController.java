b047d4f9d72ce37d4652f7c642b3f66658c78801
lucas kuraoka <eiji_karakura@jojomail>
2020-01-25 09:47:40 -0300

    add arquivos novos

---
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -0,0 +1,90 @@
+package br.com.caelum.ingresso.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.caelum.ingresso.dao.FilmeDao;
+import br.com.caelum.ingresso.dao.SalaDao;
+import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.model.form.SessaoForm;
+import br.com.caelum.ingresso.validacao.GerenciadorDeSessao;
+
+@Controller
+public class SessaoController {
+
+	@Autowired
+	private SessaoDao sessaoDao;
+
+	@Autowired
+	private SalaDao salaDao;
+
+	@Autowired
+	private FilmeDao filmeDao;
+
+	@GetMapping("/admin/sessao")
+	public ModelAndView form(@RequestParam("salaId") Integer idSala, SessaoForm form) {
+
+//		ModelAndView modelAndView = new ModelAndView("sessao/sessao");
+//
+//		modelAndView.addObject("sala", salaDao.findOne(salaId));
+//		modelAndView.addObject("filmes", filmeDao.findAll());
+//
+//		return modelAndView;
+
+		ModelAndView modelAndView = new ModelAndView("sessao/sessao");
+
+		Sala sala = salaDao.findOne(idSala);
+		modelAndView.addObject("sala", sala);
+
+		List<Filme> filmes = filmeDao.findAll();
+		modelAndView.addObject("filmes", filmes);
+
+		modelAndView.addObject("form", form);
+
+		return modelAndView;
+	}
+
+//	@PostMapping(value = "/admin/sessao")
+//	@Transactional
+//	public ModelAndView salva(@Valid SessaoForm sessaoForm, BindingResult result) {
+//
+//		if (result.hasErrors()) {
+//			return form(sessaoForm.getSalaId(), sessaoForm);
+//		}
+//
+//		Sessao sessao = sessaoForm.toSessao(salaDao, filmeDao);
+//
+//		sessaoDao.save(sessao);
+//
+//		return new ModelAndView("redirect:/admin/sala/" + sessaoForm.getSalaId() + "/sessoes");
+//	}
+
+	@PostMapping("/admin/sessao")
+	@Transactional
+	public ModelAndView salva(@Valid SessaoForm form, BindingResult result) {
+		if (result.hasErrors())
+			return form(form.getSalaId(), form);
+		Sessao sessao = form.toSessao(salaDao, filmeDao);
+		List<Sessao> sessoesDaSala = sessaoDao.buscaSessoesDaSala(sessao.getSala());
+		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoesDaSala);
+		if (gerenciador.cabe(sessao)) {
+			sessaoDao.save(sessao);
+			return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
+		}
+		return form(form.getSalaId(), form);
+	}
+
+}
